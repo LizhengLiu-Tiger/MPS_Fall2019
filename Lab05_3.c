247b78310314fcb07d200cfe6097a1d1fe83a6fb
loop2Lee <31805920+loop2Lee@users.noreply.github.com>
2019-11-10 03:44:59 -0500

    Add files via upload

---
+++ Lab05_3.c
@@ -0,0 +1,180 @@
+/*
+ * Lab05_3.c
+ *
+ *  Created on: Nov 7, 2019
+ *      Author: hyperiontiger
+ */
+
+/*
+ * LAb04_4, assembly filter
+ Created on: Oct 21, 2019
+ Author: Tiger Liu and Frank Ji
+     */
+
+#include "init.h"
+#include "math.h"
+
+void configureDAC();
+void configureADC();
+DAC_HandleTypeDef __hdac;
+DAC_ChannelConfTypeDef dac_ch;
+ADC_HandleTypeDef adc1;
+
+uint32_t x_t[3];//X array for the filter, index 0 = current reading, index 1 = previous reading
+uint16_t y_t[2];//Y array for the filter, same
+uint16_t temp;
+double inx[4];//coefficients
+double volatile var;
+//int flag_coml;
+int flag_coml;
+
+int main(void)
+{
+	/*
+	 * main  code
+	 */
+	Sys_Init();
+	configureADC();
+	configureDAC();
+	printf("\r\033[2J\033[1;1HLab5-3!\r\n");
+
+	double inx[4] = {0.3125, 0.240385, 0.312500, 0.296875};
+	/*
+	 * ADC reading
+	 */
+	while(1)
+	{
+
+		temp = x_t[1];
+		x_t[1] = x_t[0];
+		x_t[2] = temp;
+		//Take ADC input
+		HAL_ADC_Start_DMA(&adc1, x_t[0], 1);
+
+
+		//Transfer function / filter
+		double x_t0 = x_t[0];
+		double x_t1 = x_t[1];
+		double x_t2 = x_t[2];
+
+		double y_t0 = y_t[0];
+		double y_t1 = y_t[1];
+
+		double inx0 = inx[0];
+		double inx1 = inx[1];
+		double inx2 = inx[2];
+		double inx3 = inx[3];
+
+		//Apply the filter
+		y_t1 = y_t[0];
+	    asm("VMUL.F64 %P[out],%P[in1],%P[in2]"
+	           :[out] "=w" (var)
+	           :[in1] "w" (x_t0),[in2] "w" (inx0));
+	    asm volatile ("VMLA.F64 %P[out],%P[in1],%P[in2]"
+	           :[out] "+&w" (var)
+	           :[in1] "w" (x_t1),[in2] "w" (inx1));
+	    asm volatile ("VMLA.F64 %P[out],%P[in1],%P[in2]"
+	           :[out] "+&w" (var)
+	           :[in1] "w" (x_t2),[in2] "w" (inx2));
+	    asm volatile ("VMLA.F64 %P[out],%P[in1],%P[in2]"
+	           :[out] "+&w" (var)
+	           :[in1] "w" (y_t1),[in2] "w" (inx3));
+	    y_t[0] = var;
+
+	    //DAC
+		HAL_DAC_Init(&__hdac);
+		HAL_DAC_Start(&__hdac, DAC_CHANNEL_1);
+
+		HAL_DAC_SetValue(&__hdac, DAC_CHANNEL_1, DAC_ALIGN_12B_R, y_t[0]);
+		HAL_ADC_Stop_DMA(&adc1);
+		//HAL_Delay(1);
+	}
+}
+
+void configureDAC()
+{
+	/*configure the DAC
+	 *
+	 */
+	__HAL_RCC_DAC_CLK_ENABLE();
+	__hdac.Instance = DAC1;
+
+	HAL_DAC_Init(&__hdac);
+
+	dac_ch.DAC_Trigger = DAC_TRIGGER_NONE;
+	dac_ch.DAC_OutputBuffer = DAC_OUTPUTBUFFER_ENABLE;
+
+	HAL_DAC_ConfigChannel(&__hdac, &dac_ch, DAC_CHANNEL_1);
+
+}
+
+void configureADC()
+{
+	// Enable the ADC Clock.ExternalTrigConv
+	RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;
+
+	//ADC_HandleTypeDef adc1;//movetop
+	adc1.Instance = ADC1;
+	adc1.Init.Resolution = ADC_RESOLUTION_12B;
+	adc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+	adc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
+	adc1.Init.ContinuousConvMode = ENABLE;
+	adc1.Init.DMAContinuousRequests = ENABLE;
+	adc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+
+	HAL_ADC_Init(&adc1); // Initialize the ADC
+
+	// Configure the ADC channel
+
+	ADC_ChannelConfTypeDef _ADC1_IN6;
+	_ADC1_IN6.Channel = ADC_CHANNEL_6;
+	_ADC1_IN6.Offset = 0;
+	_ADC1_IN6.Rank = ADC_REGULAR_RANK_1;
+	_ADC1_IN6.SamplingTime = ADC_SAMPLETIME_3CYCLES;
+
+	HAL_ADC_ConfigChannel(&adc1, &_ADC1_IN6);
+
+}
+
+void HAL_DAC_MspInit(DAC_HandleTypeDef *hdac)
+{
+	/*configure the DAC GPIO
+	 *
+	 */
+	GPIO_InitTypeDef  GPIO_InitStruct;
+	__GPIOA_CLK_ENABLE();
+
+	GPIO_InitStruct.Pin       = GPIO_PIN_4; //Analog output PA4
+	GPIO_InitStruct.Mode      = GPIO_MODE_ANALOG;
+	GPIO_InitStruct.Pull      = GPIO_NOPULL;
+	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+}
+
+void HAL_ADC_MspInit(ADC_HandleTypeDef *hadc)
+{
+
+// GPIO init
+	GPIO_InitTypeDef  GPIO_InitStruct;
+	__GPIOA_CLK_ENABLE();
+	__GPIOC_CLK_ENABLE();
+
+
+	GPIO_InitStruct.Pin       = GPIO_PIN_6; //Analog input ADC1IN6
+	GPIO_InitStruct.Mode      = GPIO_MODE_ANALOG;
+	GPIO_InitStruct.Pull      = GPIO_PULLUP;
+	GPIO_InitStruct.Speed     = GPIO_SPEED_HIGH;
+	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+	  __HAL_RCC_GPIOC_CLK_ENABLE();
+	GPIO_InitStruct.Pin       = GPIO_PIN_7; //Pushbutton C7
+	GPIO_InitStruct.Mode      = GPIO_MODE_INPUT;
+	GPIO_InitStruct.Pull      = GPIO_PULLUP;
+	GPIO_InitStruct.Speed     = GPIO_SPEED_HIGH;
+	HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+}
+
+
+
+
---
+++ uart.c
@@ -0,0 +1,345 @@
+#include "uart.h"
+DMA_HandleTypeDef hdma_usart1_tx;
+DMA_HandleTypeDef hdma_usart1_rx;
+DMA_HandleTypeDef dma1_SPItx;
+DMA_HandleTypeDef dma1_SPIrx;
+DMA_HandleTypeDef dma2_ADC1;
+ADC_HandleTypeDef adc1;
+SPI_HandleTypeDef hspi_2;
+char input_spi2[1];
+char input_spi2s[1];
+char* ptr;
+char input[1];
+uint8_t *pData;
+int flag_coml;
+
+int rx_flag = 0;
+int tx_flag = 0;
+// Initialize Hardware Resources
+// Peripheral's clock enable
+// Peripheral's GPIO Configuration
+void HAL_UART_MspInit(UART_HandleTypeDef *huart){
+//	char *msg = "Hello STM32 Lovers! This message is transferred in DMA Mode.\r\n";
+	GPIO_InitTypeDef  GPIO_InitStruct;
+
+	if (huart->Instance == USART1) {
+		// Enable GPIO Clocks
+		__GPIOA_CLK_ENABLE();
+
+		// Initialize TX Pin
+		GPIO_InitStruct.Pin       = GPIO_PIN_9;
+		GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
+		GPIO_InitStruct.Pull      = GPIO_PULLUP;
+		GPIO_InitStruct.Speed     = GPIO_SPEED_HIGH;
+		GPIO_InitStruct.Alternate = GPIO_AF7_USART1;
+		HAL_GPIO_Init(GPIOA, &GPIO_InitStruct); //TX Config
+
+		// Initialize RX Pin
+		GPIO_InitStruct.Pin = GPIO_PIN_10;
+		HAL_GPIO_Init(GPIOA, &GPIO_InitStruct); //RX Config
+
+		// Enable UART Clocking
+		__USART1_CLK_ENABLE();
+
+		///////////////////////////////////////////////
+		// Add DMA initializations here
+		///////////////////////////////////////////////
+		__HAL_RCC_DMA2_CLK_ENABLE();
+		__HAL_RCC_DMA1_CLK_ENABLE();
+
+		  hdma_usart1_tx.Init.Channel = DMA_CHANNEL_4;
+		  hdma_usart1_tx.Instance = DMA2_Stream7 ;
+		  hdma_usart1_tx.Init.Direction = DMA_MEMORY_TO_PERIPH;
+		  hdma_usart1_tx.Init.PeriphInc = DMA_PINC_DISABLE;
+		  hdma_usart1_tx.Init.MemInc = DMA_MINC_ENABLE;
+		  hdma_usart1_tx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
+		  hdma_usart1_tx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
+		  hdma_usart1_tx.Init.Mode = DMA_NORMAL;
+		  hdma_usart1_tx.Init.Priority = DMA_PRIORITY_HIGH;
+		 // hdma_usart1_tx.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
+		  HAL_DMA_Init(&hdma_usart1_tx);
+
+		  __HAL_LINKDMA(&USB_UART,hdmatx,hdma_usart1_tx);
+
+			HAL_NVIC_SetPriority(DMA2_Stream7_IRQn,1,1);
+			HAL_NVIC_EnableIRQ(DMA2_Stream7_IRQn);
+		 // HAL_DMA_Start(&hdma_usart1_tx,  (uint32_t)msg,  (uint32_t)&huart->Instance->TDR, strlen(msg));
+		  //Enable UART in DMA mode
+		  //huart->Instance->CR3 |= USART_CR3_DMAT;
+
+
+			  hdma_usart1_rx.Init.Channel = DMA_CHANNEL_4;
+			  hdma_usart1_rx.Instance = DMA2_Stream2 ;
+			  hdma_usart1_rx.Init.Direction = DMA_PERIPH_TO_MEMORY;
+			  hdma_usart1_rx.Init.PeriphInc = DMA_PINC_DISABLE;
+			  hdma_usart1_rx.Init.MemInc = DMA_MINC_ENABLE;
+			  hdma_usart1_rx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
+			  hdma_usart1_rx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
+			  hdma_usart1_rx.Init.Mode = DMA_NORMAL;
+			  hdma_usart1_rx.Init.Priority = DMA_PRIORITY_HIGH;
+			//  hdma_usart1_rx.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
+			  HAL_DMA_Init(&hdma_usart1_rx);
+
+			  __HAL_LINKDMA(&USB_UART,hdmarx,hdma_usart1_rx);
+
+				HAL_NVIC_SetPriority(DMA2_Stream2_IRQn,0,0);
+				HAL_NVIC_EnableIRQ(DMA2_Stream2_IRQn);
+
+				HAL_NVIC_SetPriority(USART1_IRQn,2,2);
+				HAL_NVIC_EnableIRQ(USART1_IRQn);
+
+			dma1_SPItx.Instance = DMA1_Stream4;
+			dma1_SPItx.Init.Channel = DMA_CHANNEL_0;//0 for SPI TX
+			dma1_SPItx.Init.Direction = DMA_MEMORY_TO_PERIPH;
+			dma1_SPItx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
+			dma1_SPItx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE; // ??Enable both sizes?
+			dma1_SPItx.Init.PeriphInc = DMA_PINC_DISABLE;
+			dma1_SPItx.Init.MemInc = DMA_MINC_ENABLE; // ??Enable or Dis? Both?
+			dma1_SPItx.Init.Mode = DMA_NORMAL; //Any more configure?? P
+			dma1_SPItx.Init.Priority = DMA_PRIORITY_LOW;
+			HAL_DMA_Init(&dma1_SPItx);
+			__HAL_LINKDMA(&hspi_2, hdmatx, dma1_SPItx); // DMA to SPI tx
+			HAL_NVIC_SetPriority(DMA1_Stream4_IRQn,3,3);
+			HAL_NVIC_EnableIRQ(DMA1_Stream4_IRQn);
+
+
+
+			dma1_SPIrx.Instance = DMA1_Stream3;
+			dma1_SPIrx.Init.Channel = DMA_CHANNEL_0;//0 for SPI RX
+			dma1_SPIrx.Init.Direction = DMA_PERIPH_TO_MEMORY;
+			dma1_SPIrx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
+			dma1_SPIrx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE; // ??Enable both sizes?
+			dma1_SPIrx.Init.PeriphInc = DMA_PINC_DISABLE;
+			dma1_SPIrx.Init.MemInc = DMA_MINC_ENABLE; // ??Enable or Dis? Both?
+			dma1_SPIrx.Init.Mode = DMA_NORMAL; //Any more configure?? P
+			dma1_SPIrx.Init.Priority = DMA_PRIORITY_LOW;
+			HAL_DMA_Init(&dma1_SPIrx);
+			__HAL_LINKDMA(&hspi_2, hdmarx, dma1_SPIrx); // DMA to SPI rx
+			HAL_NVIC_SetPriority(DMA1_Stream3_IRQn,4,4);
+			HAL_NVIC_EnableIRQ(DMA1_Stream3_IRQn);
+
+			HAL_NVIC_SetPriority(SPI2_IRQn,5,5);
+			HAL_NVIC_EnableIRQ(SPI2_IRQn);
+
+			dma2_ADC1.Instance = DMA1_Stream4;
+			dma2_ADC1.Init.Channel = DMA_CHANNEL_0;//0 for SPI RX
+			dma2_ADC1.Init.Direction = DMA_PERIPH_TO_MEMORY;
+			dma2_ADC1.Init.MemDataAlignment = DMA_MDATAALIGN_HALFWORD;
+			dma2_ADC1.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE; // ??Enable both sizes?
+			dma2_ADC1.Init.PeriphInc = DMA_PINC_DISABLE;
+			dma2_ADC1.Init.MemInc = DMA_MINC_ENABLE; // ??Enable or Dis? Both?
+			dma2_ADC1.Init.Mode = DMA_CIRCULAR; //Any more configure?? P
+			dma2_ADC1.Init.Priority = DMA_PRIORITY_HIGH;
+			HAL_DMA_Init(&dma2_ADC1);
+			__HAL_LINKDMA(&adc1, DMA_Handle, dma2_ADC1); // DMA to SPI rx
+			HAL_NVIC_SetPriority(DMA2_Stream4_IRQn,0,0);
+			HAL_NVIC_EnableIRQ(DMA2_Stream4_IRQn);
+
+			HAL_NVIC_SetPriority(ADC_IRQn,7,7);
+			HAL_NVIC_EnableIRQ(ADC_IRQn);
+
+	} else if (huart->Instance == USART6) {
+		// Enable GPIO Clocks
+		__GPIOC_CLK_ENABLE();
+
+		// Initialize TX Pin
+		GPIO_InitStruct.Pin       = GPIO_PIN_6;
+		GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
+		GPIO_InitStruct.Pull      = GPIO_PULLUP;
+		GPIO_InitStruct.Speed     = GPIO_SPEED_HIGH;
+		GPIO_InitStruct.Alternate = GPIO_AF8_USART6;
+		HAL_GPIO_Init(GPIOC, &GPIO_InitStruct); //TX Config
+
+		// Initialize RX Pin
+		GPIO_InitStruct.Pin = GPIO_PIN_7;
+		HAL_GPIO_Init(GPIOC, &GPIO_InitStruct); //RX Config
+
+		// Enable UART Clocking
+		__USART6_CLK_ENABLE();
+
+	}
+}
+
+//UART Initialization
+void initUart(UART_HandleTypeDef* Uhand, uint32_t Baud, USART_TypeDef* Tgt) {
+	Uhand->Instance        = Tgt;
+
+	Uhand->Init.BaudRate   = Baud;
+	Uhand->Init.WordLength = UART_WORDLENGTH_8B;
+	Uhand->Init.StopBits   = UART_STOPBITS_1;
+	Uhand->Init.Parity     = UART_PARITY_NONE;
+	Uhand->Init.Mode       = UART_MODE_TX_RX;
+	Uhand->Init.HwFlowCtl  = UART_HWCONTROL_NONE;
+
+	HAL_UART_Init(Uhand);
+}
+
+/* ============================================================================
+
+Reassigning _write and _read to USB_UART by default.
+
+Note the following for an explanation of syntax:
+
+HAL_UART_Transmit(UART_HandleTypeDef *huart, uint8_t *pData, uint16_t Size, uint32_t Timeout)
+
+HAL_UART_Receive(UART_HandleTypeDef *huart, uint8_t *pData, uint16_t Size, uint32_t Timeout)
+
+============================================================================= */
+
+///////////////////////////////////////////////
+// Change _write() and _read() to use DMAs
+///////////////////////////////////////////////
+
+// Make printf(), putchar(), etc. default to work over USB UART
+int _write(int file, char *ptr, int len) {
+
+	HAL_UART_Transmit_DMA(&USB_UART, (uint8_t*) ptr, len);
+	while(tx_flag != 1);
+	tx_flag = 0;
+
+	return len;
+}
+
+// Make scanf(), getchar(), etc. default to work over USB UART
+int _read(int file, char *ptr, int len) {
+	*ptr = 0x00; // Clear the character buffer because scanf() is finicky
+	len = 1; // Again because of scanf's finickiness, len must = 1
+	HAL_UART_Receive_DMA(&USB_UART, (uint8_t*) ptr, len);
+	while(rx_flag != 1);
+	rx_flag = 0;
+
+	return len;
+}
+
+/* ============================================================================
+
+Custom UART functions that can be mapped to any UART of your choice when called
+
+Note the following for an explanation of syntax:
+
+HAL_UART_Transmit(UART_HandleTypeDef *huart, uint8_t *pData, uint16_t Size, uint32_t Timeout)
+
+HAL_UART_Receive(UART_HandleTypeDef *huart, uint8_t *pData, uint16_t Size, uint32_t Timeout)
+
+============================================================================= */
+
+// Print a string on the specified UART. (Basically a redirectable puts)
+void uart_print(UART_HandleTypeDef *huart, char* string) {
+    HAL_UART_Transmit_DMA(huart, (uint8_t*) string, strlen((const char*)string));
+}
+
+// Get one character
+// 'echo' means enable (1) or disable (0) echoing of characters
+char uart_getchar(UART_HandleTypeDef *huart, uint8_t echo) {
+	//char input[1];
+	while(HAL_UART_Receive_DMA(huart, (uint8_t *)input, 1) != HAL_OK);
+	if (echo) while(HAL_UART_Transmit_DMA(huart, (uint8_t*) input, 1) != HAL_OK);
+	return (char)input[0];
+}
+
+// Send one character
+void uart_putchar(UART_HandleTypeDef *huart, char * input) {
+	HAL_UART_Transmit_DMA(huart, (uint8_t*) input, 1);
+}
+
+// Collects characters until size limit or an endline is recieved
+// Returns number of characters collected
+// 'max size' should match the size of the array or be smaller
+int uart_getline(UART_HandleTypeDef *huart, char * input, int max_size)
+{
+  char single;
+  int i = 0;
+
+	while(1)
+	{
+		if (i > max_size)
+      {
+				break;
+			}
+
+		single = uart_getchar(huart, 1); // Get one character
+
+  	if (single == '\n' || single == '\r') // Unix & co. use \n, Windows uses \r\n
+			break;
+  	else
+			input[i] = single;
+  	i++;
+	}
+  return i; // The way this is set up, it will always report the wrong size
+}
+
+
+void USART1_IRQHandler(void){
+	HAL_UART_IRQHandler(&USB_UART);
+}
+
+
+void SPI2_IRQHandler(void){
+	HAL_SPI_IRQHandler(&hspi_2);
+}
+
+
+void DMA2_Stream2_IRQHandler(void){
+	HAL_DMA_IRQHandler(&hdma_usart1_rx);
+}
+
+void DMA2_Stream4_IRQHandler(void){
+	HAL_DMA_IRQHandler(&dma2_ADC1);
+}
+
+
+void DMA2_Stream7_IRQHandler(void){
+	HAL_DMA_IRQHandler(&hdma_usart1_tx);
+}
+
+void DMA1_Stream3_IRQHandler(void){
+	HAL_DMA_IRQHandler(&dma1_SPIrx);
+}
+
+
+void DMA1_Stream4_IRQHandler(void){
+	HAL_DMA_IRQHandler(&dma1_SPItx);
+}
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *haurt){
+	rx_flag = 1;
+}
+
+
+void HAL_UART_TxCpltCallback(UART_HandleTypeDef *haurt){
+	tx_flag = 1;
+}
+
+void HAL_SPI_RxCpltCallback(SPI_HandleTypeDef *hspi){
+//	rx_flag = 1;
+}
+
+//void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *hadc){
+//	flag_coml = 1;
+//}
+
+
+void HAL_SPI_TxCpltCallback(SPI_HandleTypeDef *hspi){
+//	tx_flag = 1;
+}
+
+void ADC_IRQHandler(void){
+	HAL_ADC_IRQHandler(&adc1);
+}
+
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *hadc){
+	flag_coml = 1;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
