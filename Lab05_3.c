d5d2bef3451e16dab6aa8871b414316d191956c4
loop2Lee <31805920+loop2Lee@users.noreply.github.com>
2019-11-11 19:23:15 -0500

    Update Lab05_3.c

--- Lab05_3.c
+++ Lab05_3.c
@@ -1,110 +1,63 @@
 /*
- * Lab05_3.c
- *
- *  Created on: Nov 7, 2019
- *      Author: hyperiontiger
+ * Lab04_4 C filter
+ * Created on Oct 21, 2019
+ * Author: Tiger Liu and Frank Ji
  */
-
-/*
- * LAb04_4, assembly filter
- Created on: Oct 21, 2019
- Author: Tiger Liu and Frank Ji
-     */
 
 #include "init.h"
 #include "math.h"
 
-void configureDAC();
-void configureADC();
-DAC_HandleTypeDef __hdac;
-DAC_ChannelConfTypeDef dac_ch;
-ADC_HandleTypeDef adc1;
+DMA_HandleTypeDef hdma_adc; //DMA handle for the ADC1
 
-uint32_t x_t[3];//X array for the filter, index 0 = current reading, index 1 = previous reading
-uint16_t y_t[2];//Y array for the filter, same
-uint16_t temp;
-double inx[4];//coefficients
-double volatile var;
-//int flag_coml;
-int flag_coml;
+void configureADC();
+
+//define global variables
+int flag =0;
+volatile uint32_t raw_value;
+int prev1 = 0;
+int prev2 = 0;
+float prev_result = 0;
+int temp;
+int output_value;
+float result;
+
+int adc_flag = 0;
+
+float vol;
+
+//define adc and dac
+ADC_HandleTypeDef adc1;
+DAC_HandleTypeDef hdac;
+
+// Main Execution Loop
 
 int main(void)
 {
-	/*
-	 * main  code
-	 */
+	//Initialize the system
 	Sys_Init();
 	configureADC();
 	configureDAC();
+
 	printf("\r\033[2J\033[1;1HLab5-3!\r\n");
+    	HAL_DAC_Start(&hdac, DAC_CHANNEL_1);
+	HAL_ADC_Start_DMA(&adc1, &raw_value, 1);//Start ADC conversion in DMA mode
 
-	double inx[4] = {0.3125, 0.240385, 0.312500, 0.296875};
-	/*
-	 * ADC reading
-	 */
-	while(1)
-	{
+	  while(1)
+	  {
+		  		//store previous result
+			  	prev_result = result;
+			  	temp = prev1;
+		  		prev1 = raw_value;
+		  		prev2 = temp;
+			  	//clear  flag
+				while(!adc_flag);
+				adc_flag = 0;
 
-		temp = x_t[1];
-		x_t[1] = x_t[0];
-		x_t[2] = temp;
-		//Take ADC input
-		HAL_ADC_Start_DMA(&adc1, (uint32_t*)x_t[0], 1);
+				//Implement the filter value and output through DAC
+			  	result = 0.3125*(float)raw_value+0.240385*(float)prev1+0.3125*(float)prev2+0.296875*(float)prev_result;
+			  	HAL_DAC_SetValue(&hdac, DAC_CHANNEL_1, DAC_ALIGN_12B_R, (int)result);
 
-
-		//Transfer function / filter
-		double x_t0 = x_t[0];
-		double x_t1 = x_t[1];
-		double x_t2 = x_t[2];
-
-		double y_t0 = y_t[0];
-		double y_t1 = y_t[1];
-
-		double inx0 = inx[0];
-		double inx1 = inx[1];
-		double inx2 = inx[2];
-		double inx3 = inx[3];
-
-		//Apply the filter
-		y_t1 = y_t[0];
-	    asm("VMUL.F64 %P[out],%P[in1],%P[in2]"
-	           :[out] "=w" (var)
-	           :[in1] "w" (x_t0),[in2] "w" (inx0));
-	    asm volatile ("VMLA.F64 %P[out],%P[in1],%P[in2]"
-	           :[out] "+&w" (var)
-	           :[in1] "w" (x_t1),[in2] "w" (inx1));
-	    asm volatile ("VMLA.F64 %P[out],%P[in1],%P[in2]"
-	           :[out] "+&w" (var)
-	           :[in1] "w" (x_t2),[in2] "w" (inx2));
-	    asm volatile ("VMLA.F64 %P[out],%P[in1],%P[in2]"
-	           :[out] "+&w" (var)
-	           :[in1] "w" (y_t1),[in2] "w" (inx3));
-	    y_t[0] = var;
-
-	    //DAC
-		HAL_DAC_Init(&__hdac);
-		HAL_DAC_Start(&__hdac, DAC_CHANNEL_1);
-
-		HAL_DAC_SetValue(&__hdac, DAC_CHANNEL_1, DAC_ALIGN_12B_R, y_t[0]);
-		HAL_ADC_Stop_DMA(&adc1);
-		//HAL_Delay(1);
-	}
-}
-
-void configureDAC()
-{
-	/*configure the DAC
-	 *
-	 */
-	__HAL_RCC_DAC_CLK_ENABLE();
-	__hdac.Instance = DAC1;
-
-	HAL_DAC_Init(&__hdac);
-
-	dac_ch.DAC_Trigger = DAC_TRIGGER_NONE;
-	dac_ch.DAC_OutputBuffer = DAC_OUTPUTBUFFER_ENABLE;
-
-	HAL_DAC_ConfigChannel(&__hdac, &dac_ch, DAC_CHANNEL_1);
+	  }
 
 }
 
@@ -115,16 +68,17 @@ void configureADC()
 
 	//ADC_HandleTypeDef adc1;//movetop
 	adc1.Instance = ADC1;
+	adc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
 	adc1.Init.Resolution = ADC_RESOLUTION_12B;
 	adc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
 	adc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
-	adc1.Init.ContinuousConvMode = DISABLE;
-	adc1.Init.DiscontinuousConvMode = DISABLE;
 	adc1.Init.DMAContinuousRequests = ENABLE;
-	adc1.Init.EOCSelection = ADC_EOC_SEQ_CONV;
-
+	adc1.Init.ContinuousConvMode = ENABLE;
+	adc1.Init.DiscontinuousConvMode = DISABLE;
+	adc1.Init.NbrOfConversion = 1;
+	adc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
 	HAL_ADC_Init(&adc1); // Initialize the ADC
-
+	
 	// Configure the ADC channel
 
 	ADC_ChannelConfTypeDef _ADC1_IN6;
@@ -132,25 +86,31 @@ void configureADC()
 	_ADC1_IN6.Offset = 0;
 	_ADC1_IN6.Rank = ADC_REGULAR_RANK_1;
 	_ADC1_IN6.SamplingTime = ADC_SAMPLETIME_480CYCLES;
-
 	HAL_ADC_ConfigChannel(&adc1, &_ADC1_IN6);
 
+	__HAL_RCC_DMA2_CLK_ENABLE();
+
+	hdma_adc.Init.Channel = DMA_CHANNEL_0;
+	hdma_adc.Instance = DMA2_Stream0 ;
+	hdma_adc.Init.Direction = DMA_PERIPH_TO_MEMORY;
+	hdma_adc.Init.PeriphInc = DMA_PINC_DISABLE;
+	hdma_adc.Init.MemInc = DMA_MINC_DISABLE;
+	hdma_adc.Init.PeriphDataAlignment = DMA_PDATAALIGN_WORD;
+	hdma_adc.Init.MemDataAlignment = DMA_MDATAALIGN_WORD;
+	hdma_adc.Init.Mode = DMA_CIRCULAR;
+	hdma_adc.Init.Priority = DMA_PRIORITY_HIGH;
+	// hdma_usart1_tx.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
+	__HAL_LINKDMA(&adc1,DMA_Handle,hdma_adc);
+	HAL_DMA_Init(&hdma_adc);
+	
+	HAL_NVIC_SetPriority(DMA2_Stream0_IRQn,0,0);
+	HAL_NVIC_EnableIRQ(DMA2_Stream0_IRQn);
+
+	HAL_NVIC_SetPriority(ADC_IRQn,1,1);
+	HAL_NVIC_EnableIRQ(ADC_IRQn);
+
 }
 
-void HAL_DAC_MspInit(DAC_HandleTypeDef *hdac)
-{
-	/*configure the DAC GPIO
-	 *
-	 */
-	GPIO_InitTypeDef  GPIO_InitStruct;
-	__GPIOA_CLK_ENABLE();
-
-	GPIO_InitStruct.Pin       = GPIO_PIN_4; //Analog output PA4
-	GPIO_InitStruct.Mode      = GPIO_MODE_ANALOG;
-	GPIO_InitStruct.Pull      = GPIO_NOPULL;
-	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-}
 
 void HAL_ADC_MspInit(ADC_HandleTypeDef *hadc)
 {
@@ -159,7 +119,6 @@ void HAL_ADC_MspInit(ADC_HandleTypeDef *hadc)
 	GPIO_InitTypeDef  GPIO_InitStruct;
 	__GPIOA_CLK_ENABLE();
 	__GPIOC_CLK_ENABLE();
-
 
 	GPIO_InitStruct.Pin       = GPIO_PIN_6; //Analog input ADC1IN6
 	GPIO_InitStruct.Mode      = GPIO_MODE_ANALOG;
@@ -177,5 +136,42 @@ void HAL_ADC_MspInit(ADC_HandleTypeDef *hadc)
 }
 
 
+void configureDAC()
+{
+	  DAC_ChannelConfTypeDef sConfig;
+	  GPIO_InitTypeDef GPIO_InitStruct;
+
+	  __HAL_RCC_DAC_CLK_ENABLE();
+
+	  /* DAC Initialization  */
+	  hdac.Instance = DAC1;
+	  HAL_DAC_Init(&hdac);
+
+	  /**DAC channel OUT1 config */
+	  sConfig.DAC_Trigger = DAC_TRIGGER_NONE;
+	  sConfig.DAC_OutputBuffer = DAC_OUTPUTBUFFER_ENABLE;
+	  HAL_DAC_ConfigChannel(&hdac, &sConfig, DAC_CHANNEL_1);
+
+	  /* DAC GPIO Configuration
+	     PA4     ------> DAC_OUT1
+	  */
+	  GPIO_InitStruct.Pin = GPIO_PIN_4;
+	  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+	  GPIO_InitStruct.Pull = GPIO_NOPULL;
+	  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+}
+
+
+void DMA2_Stream0_IRQHandler(void){
+	HAL_DMA_IRQHandler(&hdma_adc);
+}
+
+void ADC1_IRQHandler(void){
+	HAL_ADC_IRQHandler(&adc1);
+}
+
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc){
+	adc_flag = 1;
+}
 
 
